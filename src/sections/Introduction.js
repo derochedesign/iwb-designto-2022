e5a4697ec05d6874ab54577601e9c3b5f04f3243
Aaron <aaron@deroche.design>
2022-01-26 03:27:02 -0500

    content updates

--- src/sections/Introduction.js
+++ src/sections/Introduction.js
@@ -20,7 +20,7 @@ const Introduction = props => {
         </div>
         <div className="item-list">
           <p>Designing for a climate ready community means more than just envisioning a sustainable future. Designing for climate ready communities requires the recognition and integration of the most underrepresented stakeholders in climate discourse: those displaced by environmental risk.</p>
-          <p>This digital tool represents how human mobility is implicated in the crossroads of climate change. Uncover nodes and discover sites of intervention for designers and non-designers alike to set the stage for a climate-ready future. This method will make visible the implicit connections that drive one of the most concerning environmental impacts: the loss of home and community.</p>
+          <p>This learning tool represents how human mobility is implicated in the crossroads of climate change. Uncover nodes and discover sites of intervention for designers and non-designers alike to set the stage for a climate-ready future. This tool will make visible the implicit connections that drive one of the most concerning environmental impacts: the loss of home and community.</p>
         </div>
       </div>
     </SectionWrapper>
--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -15,13 +15,14 @@ const Landing = props => {
       <div className="landing-overlay" data-active={props.showOverlay}>
         <Countdown />
         <div className="center-align">
-          <h6 className="quote">&#x201C; Climate Migration hotspots will emerge as early as 2030 &#x201D;</h6>
-          <p className="small">Groundswell Report on Climate Migration</p>
+          <p>Due to the escalating effects of climate change, human migration will surge as early as 2030.</p>
+          {/* <h6 className="quote">&#x201C; Climate Migration hotspots will emerge as early as 2030 &#x201D;</h6>
+          <p className="small">Groundswell Report on Climate Migration</p> */}
         </div>
         <button onClick={() => props.setShowOverlay(false)}>Connect the Dots</button>
       </div>
       <div className="item-list">
-        <h1 className="alt">Escalating pressure on these hotspots will displace 216 million people in the ensuing decades.</h1>
+        <h1 className="alt">Immediate disasters, such as floods, wildfires, and extreme temperatures, in addition to steady environmental deterioration such as air pollution and desertification, will displace 216 million people in the ensuing decades.</h1>
         <div>
           <h1 className="alt">When our homes are no longer habitable,</h1>
           <h1 className="alt">where will we go?</h1>
